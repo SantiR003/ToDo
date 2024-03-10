c865d1a3eff817c60e8b1170df43fc1a01523a3c
SantiR003 <83555664+SantiR003@users.noreply.github.com>
2024-03-10 18:04:01 -0300

    first commit

---
+++ springboot-to-do/src/test/java/com/santi/proyecto/springboottodo/SpringbootToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santi.proyecto.springboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
